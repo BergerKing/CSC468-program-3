4b4a73ac13af744d47b1fa06fa6d9935a6cbebd4
jandanderson <jason.a.anderson@mines.sdsmt.edu>
2016-04-21 08:55:07 -0600

    Added Colloquium and Research skeleton.

---
+++ Colloquium.php
@@ -0,0 +1,45 @@
+<html>
+<title> Colloquium </title>
+<head>
+  <center>
+    <h1>Colloquium Schedule</h1>
+  </center>
+  <a href=Research.php>Research</a>
+</head>
+<body>
+  <div>
+    <center>
+      <?php
+        $Title = "History of Runeterra";
+        $img = "";
+        $Time = "7pm";
+        $Date = "February 30th 2017";
+        $Location = "Runeterra Univeristy - Museum of Natural History";
+        $Presentor = "Ezreal";
+        $Description = "Describing the changes and effects of the environment\n on Runterra";
+        echo "<hr>";
+        echo "<p>$Title";
+        //Presenter photo here?
+        echo "<p>$Time, $Date, $Location";
+        echo "<p>$Presentor";
+        echo "<p>$Description";
+      ?>
+      <?php
+        $Title = "The tale of a Door";
+        $img = "";
+        $Time = "Noon";
+        $Date = "April 31th 2017";
+        $Location = "Freljord Inn";
+        $Presentor = "Braum";
+        $Description = "A tale of an impenetrable door and a young troll boy.";
+        echo "<hr>";
+        echo "<p>$Title";
+        //Presenter photo here?
+        echo "<p>$Time, $Date, $Location";
+        echo "<p>$Presentor";
+        echo "<p>$Description";
+      ?>
+    </center>
+  <div>
+</body>
+</html>
---
+++ Research.php
@@ -0,0 +1,41 @@
+<html>
+<title> Research </title>
+<head>
+  <center>
+    <h1>Research</h1>
+  </center>
+  <a href=Colloquium.php>Colloquium</a>
+</head>
+<body>
+  <div>
+    <center>
+      <?php
+        $PresName = "Tahm Kench";
+        $PresType = "Doctorate of Culinary Arts";
+        $img = "";
+        $DegLevel = "PhD";
+        $University = "Runeterra Univeristy";
+        $Research = "Fish, Seafood, and Champions";
+        echo "<hr>";
+        echo "<p>$PresName: $PresType";
+        //Presenter photo here?
+        echo "<p>$DegLevel, $University";
+        echo "<p>Research Areas: $Research";
+      ?>
+      <?php
+        $PresName = "Bard";
+        $PresType = "Vagabond";
+        $img = "";
+        $DegLevel = "Diploma";
+        $University = "Valoran Univeristy";
+        $Research = "Magical Journey";
+        echo "<hr>";
+        echo "<p>$PresName: $PresType";
+        //Presenter photo here?
+        echo "<p>$DegLevel, $University";
+        echo "<p>Research Areas: $Research";
+      ?>
+    </center>
+  <div>
+</body>
+</html>
