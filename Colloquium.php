530549afc78be16026fcef2f176a18a4d6304f69
unknown <7047629@T-SMD1017194.SDSMT.local>
2016-04-21 13:35:08 -0600

    Updated coloquium

--- Colloquium.php
+++ Colloquium.php
@@ -1,4 +1,98 @@
 <html>
+
+<?php
+
+
+class Colloquium {
+    var $title;
+    var $time;
+    var $date;
+	var $location;
+	var $presentor;
+	var $description;
+	var $image;
+	
+	function Colloquium ($aa)
+	{
+		foreach ($aa as $k=>$v)
+            $this->$k = $aa[$k];
+	}
+	
+}
+
+function readDatabase($filename) 
+{
+    // read the XML database of aminoacids
+    $data = implode("", file($filename));
+    $parser = xml_parser_create();
+    xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+    xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+    xml_parse_into_struct($parser, $data, $values, $tags);
+    xml_parser_free($parser);
+
+    // loop through the structures
+    foreach ($tags as $key=>$val) {
+        if ($key == "molecule") {
+            $molranges = $val;
+            // each contiguous pair of array entries are the 
+            // lower and upper range for each molecule definition
+            for ($i=0; $i < count($molranges); $i+=2) {
+                $offset = $molranges[$i] + 1;
+                $len = $molranges[$i + 1] - $offset;
+                $tdb[] = parseMol(array_slice($values, $offset, $len));
+            }
+        } else {
+            continue;
+        }
+    }
+    return $tdb;
+}
+
+function parseMol($mvalues) 
+{
+    for ($i=0; $i < count($mvalues); $i++) {
+        $mol[$mvalues[$i]["tag"]] = $mvalues[$i]["value"];
+    }
+//    return new AminoAcid($mol);
+	return new Colloquium($mol);
+
+}
+
+function addNewResearch ()
+{
+	$file = 'Research.xml';
+
+	$xml = simplexml_load_file($file);
+
+	$galleries = $xml->moldb;
+
+	$gallery = $galleries->addChild('molecule');
+	$gallery->addChild('name', 'info');
+	$gallery->addChild('title', 'info');
+	$gallery->addChild('degree', 'info');
+	$gallery->addChild('university', 'info');
+	$gallery->addChild('research', 'info');
+	$gallery->addChild('website', 'info');
+	$gallery->addChild('image', 'info');
+	
+//	print($xml);
+	$xml->asXML($file);
+}
+
+/*
+//$db = readDatabase("moldb.xml");
+$db = readDatabase("Research.xml");
+echo "** Database of Research objects:<br>";
+print_r($db[0]);
+echo("<br><br>");
+print_r($db[1]);
+echo("<br><br>");
+print_r($db[1]->title);
+addNewResearch();
+*/
+
+?>
+
 <title> Colloquium </title>
 <head>
   <center>
@@ -10,36 +104,20 @@
   <div>
     <center>
       <?php
-        $Title = "History of Runeterra";
-        $img = "";
-        $Time = "7pm";
-        $Date = "February 30th 2017";
-        $Location = "Runeterra Univeristy - Museum of Natural History";
-        $Presentor = "Ezreal";
-        $Description = "Describing the changes and effects of the environment\n on Runterra";
-        echo "<hr>";
-        echo "<p>$Title";
-        //Presenter photo here?
-        echo "<p>$Time, $Date, $Location";
-        echo "<p>$Presentor";
-        echo "<p>$Description";
-      ?>
-      <?php
-        $Title = "The tale of a Door";
-        $img = "";
-        $Time = "Noon";
-        $Date = "April 31th 2017";
-        $Location = "Freljord Inn";
-        $Presentor = "Braum";
-        $Description = "A tale of an impenetrable door and a young troll boy.";
-        echo "<hr>";
-        echo "<p>$Title";
-        //Presenter photo here?
-        echo "<p>$Time, $Date, $Location";
-        echo "<p>$Presentor";
-        echo "<p>$Description";
+	  
+		$db = readDatabase("Colloquium.xml");
+	  
+		foreach($db as $col)
+		{
+			echo "<hr>";
+			echo "<p>$col->title</p>";
+			//Presenter photo here?
+			echo "<p>$col->time, $col->date, $col->location</p>";
+			echo "<p>$col->presentor</p>";
+			echo "<p>$col->description</p>";
+		}
       ?>
     </center>
-  <div>
+  </div>
 </body>
 </html>
